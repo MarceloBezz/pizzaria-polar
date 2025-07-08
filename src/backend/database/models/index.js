cdf2f06c99d542a9fb96070b9aa3a13a085383fa
Marcelo <mdbezerra16@gmail.com>
2025-07-07 22:01:42 -0300

    Configuração sequelize

--- src/backend/database/models/index.js
+++ src/backend/database/models/index.js
@@ -9,9 +9,13 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.js')[env];
 const db = {};
 
+console.log('Ambiente:', env);
+console.log('Configuração carregada:', config);
+
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  // sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(config)
 } else {
   // sequelize = new Sequelize(config.database, config.username, config.password, config);
   sequelize = new Sequelize(config)
