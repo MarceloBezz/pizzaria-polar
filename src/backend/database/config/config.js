17d3b8cea2dc8d4377c077f0d31cfbf6224fd64c
Marcelo <mdbezerra16@gmail.com>
2025-07-07 21:39:57 -0300

    Configuração do ambiente production

--- src/backend/database/config/config.js
+++ src/backend/database/config/config.js
@@ -15,6 +15,15 @@ module.exports = {
   },
 
   production: {
-    
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    server: process.env.DB_HOST,
+    dialect: 'mssql', 
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    }
   }
 };
